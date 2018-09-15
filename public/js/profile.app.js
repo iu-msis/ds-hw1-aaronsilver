626874d67fda90ce2a1d08bd1b50fd36df5420bc
Aaron Silver <aasilver@indiana.edu>
2018-09-14 22:14:17 -0400

    Fetch Data

--- public/js/profile.app.js
+++ public/js/profile.app.js
@@ -1,6 +1,36 @@
-var profileapp = new Vue({
+var profileApp = new Vue({
   el: '#profile',
   data: {
-    message: 'Hello Vue!'
+
+    tasks: [
+      {
+
+
+      }
+    ]
+  },
+
+computed: {
+age: function () {
+
+  return moment().diff(moment(this.tasks.results[0].dob.date), 'years')
+},
+
+},
+  methods: {
+
+    fetchTasks () {
+      fetch('https://randomuser.me/api/')
+      .then( response => response.json() )
+      .then( json => {profileApp.tasks = json} )
+      .catch( err => {
+        console.log('TASK FETCH ERROR:');
+        console.log(err);
+      })
+    },
+
+  },
+  created () {
+    this.fetchTasks();
   }
 })
