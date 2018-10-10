3abf933d749d780c77e4f310175de78ed7f6ed7b
Aaron Silver <aasilver@indiana.edu>
2018-10-10 08:56:34 -0400

    Revert "attempt 9"
    This reverts commit 7c08ff20c09ca5c88e298ef064762057ef0f488f.

--- app/data/comment-table.sql
+++ app/data/comment-table.sql
@@ -4,9 +4,9 @@
 DROP TABLE IF EXISTS Comments;
 CREATE TABLE Comments (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  comment VARCHAR(31) NOT NULL
+  comment VARCHAR(31) NOT NULL,
 );
 
-INSERT INTO Comments (id, comment) VALUES (1, 'Hello world',);
-INSERT INTO Comments (id, comment) VALUES (2, 'D&S is awesome');
-INSERT INTO Comments (id, comment) VALUES (3, 'Smile Face');
+INSERT INTO Teams (id, comment) VALUES (1, 'Hello world',);
+INSERT INTO Teams (id, comment) VALUES (2, 'D&S is awesome');
+INSERT INTO Teams (id, comment) VALUES (3, 'Smile Face');
