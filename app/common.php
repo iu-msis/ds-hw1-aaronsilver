7876e91f53b958ea7c7a8cd3ee89c18514f21ae0
Aaron Silver <aasilver@indiana.edu>
2018-10-10 06:00:34 -0400

    Get/Post Attempt 2

--- app/common.php
+++ app/common.php
@@ -6,6 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'
 && stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
   $_POST = json_decode(file_get_contents('php://input'), true);
 }
+
 require 'environment.php';
 
 /** MODELS **/
