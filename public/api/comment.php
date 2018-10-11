3c760d44e355792ab3c42f275eca53b2e643b7a4
Aaron Silver <aasilver@indiana.edu>
2018-10-11 16:12:09 -0400

    Attempt 18

--- public/api/comment.php
+++ public/api/comment.php
@@ -5,10 +5,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'commentPost.php';
   exit;
 }
+$id = intval($_GET['id'] ?? 0);
 
-
-
-$comment = Comments::findAll();
 // 1. Go to the database and get all work associated with the $taskId
 $commentArr = Comments::getCommentById($id);
 // 2. Convert to JSON
