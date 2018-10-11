8204aa839daa5b66c16fdf9c8ead0c205c06af06
Aaron Silver <aasilver@indiana.edu>
2018-10-11 14:56:27 -0400

    Attempt 11

--- public/api/comment.php
+++ public/api/comment.php
@@ -10,7 +10,7 @@ if ($id < 1) {
   throw new Exception('Invalid ID');
 }
 // 1. Go to the database and get all work associated with the $taskId
-$commentArr = Comments::getCommentsById($id);
+$commentArr = Comments::getCommentById($id);
 // 2. Convert to JSON
 $json = json_encode($commentArr, JSON_PRETTY_PRINT);
 // 3. Print
