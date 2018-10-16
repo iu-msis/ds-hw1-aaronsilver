ce7434b3d64301fed24a382ff2d9c3ffb9308a68
Aaron Silver <aasilver@indiana.edu>
2018-10-16 17:15:57 -0400

    Revert "Attempt 18"
    This reverts commit 3c760d44e355792ab3c42f275eca53b2e643b7a4.

--- public/api/comment.php
+++ public/api/comment.php
@@ -5,8 +5,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   require 'commentPost.php';
   exit;
 }
-$id = intval($_GET['id'] ?? 0);
 
+
+
+$comment = Comments::findAll();
 // 1. Go to the database and get all work associated with the $taskId
 $commentArr = Comments::getCommentById($id);
 // 2. Convert to JSON
